31e8574bb93ab01ec25d8b8ec8af48faf245a48a
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-21 12:44:07 -0600

    Hydro data

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,10 +1,10 @@
-// class Hydration {
-//   constructor(hydrationObj) {
-//     this.userID = hydrationObj.userID;
-//     this.date = hydrationObj.date;
-//     this.numOunces = hydrationObj.numOunces;
-//   }
-// }
-//
-//
-// export default Hydration;
+class Hydration {
+  constructor(hydrationObj) {
+    this.userID = hydrationObj.userID;
+    this.date = hydrationObj.date;
+    this.numOunces = hydrationObj.numOunces;
+  }
+}
+
+
+export default Hydration;
