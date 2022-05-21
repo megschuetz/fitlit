4be81c364dbb8afd3f2ecfd327ebcfb2d41085e9
“Corinne <corinne.canavan@gmail.com>
2022-05-21 15:27:17 -0600

    Add semicolons for consistency

--- src/scripts.js
+++ src/scripts.js
@@ -30,7 +30,7 @@ Promise.all([fetchUserData(), fetchUserActivity(), fetchUserSleep(), fetchUserHy
   .then(data => {
     console.log(data)
     userDataHelper(data[0].userData);
-    activityDataHelper(data[1].activityData)
+    activityDataHelper(data[1].activityData);
     sleepDataHelper(data[2].sleepData);
     hydrationDataHelper(data[3].hydrationData);
 });
@@ -56,16 +56,16 @@ function hydrationDataHelper(data) {
 
 function sleepDataHelper(data) {
   const sleepRepo = new SleepRepository(data);
-  displaySleepInfo(displayedUsersID, sleepRepo)
+  displaySleepInfo(displayedUsersID, sleepRepo);
 };
 
 function activityDataHelper(data) {
-  const activityRepo = new Activity(data)
-  displayActivityInfo(activityRepo)
+  const activityRepo = new Activity(data);
+  displayActivityInfo(activityRepo);
 };
 
-let firstName = document.getElementById('first-name')
-let lastName = document.getElementById('last-name')
+let firstName = document.getElementById('first-name');
+let lastName = document.getElementById('last-name');
 
 
 //  DOM
@@ -74,31 +74,29 @@ function displayUserInfo(user, userRepo) {
     return userRepo.getUserById(friend).name;
   });
   welcomeName.innerHTML = `WELCOME, ${user.getUserFirstName().toUpperCase()}`;
-  firstName.innerText = `${user.getUserFirstName().toUpperCase()}`
-  lastName.innerText = `${user.getUserLastName().toUpperCase()}`
+  firstName.innerText = `${user.getUserFirstName().toUpperCase()}`;
+  lastName.innerText = `${user.getUserLastName().toUpperCase()}`;
   stepGoal.innerText = `${user.dailyStepGoal}`;
   email.innerText = `${user.email}`;
   friends.innerText = `${getFriendsNames}`;
   avgStepGoal.innerText = `${userRepo.calculateAvgStepGoal()}`;
-
 };
 
-function displayActivityInfo(activityRepo){
-  const allUsersActivity = activityRepo.findUser(displayedUsersID)
-  stepsTaken.innerText = `Steps: ${allUsersActivity[allUsersActivity.length -1].numSteps}`
-  minsActive.innerText = `Minutes Active: ${allUsersActivity[allUsersActivity.length -1].minutesActive}`
-  flights.innerText = `Flights Taken: ${allUsersActivity[allUsersActivity.length -1].flightsOfStairs}`
+function displayActivityInfo(activityRepo) {
+  const allUsersActivity = activityRepo.findUser(displayedUsersID);
+  stepsTaken.innerText = `Steps: ${allUsersActivity[allUsersActivity.length -1].numSteps}`;
+  minsActive.innerText = `Minutes Active: ${allUsersActivity[allUsersActivity.length -1].minutesActive}`;
+  flights.innerText = `Flights Taken: ${allUsersActivity[allUsersActivity.length -1].flightsOfStairs}`;
 };
 
 function displaySleepInfo(id, sleepRepo) {
   const allUserData = sleepRepo.getAllUserData(id);
   const sleep = sleepRepo.makeNewSleep(id, allUserData);
 
-  lastSleep.innerText = `Last Night: ${sleep.latest.hoursSlept}`
-  weeklySleep.innerText = `Weekly Avg: ${sleep.calculateAvg(sleep.latest.date, "hoursSlept")}`
-  avgSleep.innerHTML = `Average Hours Slept: ${sleep.avgHoursSlept}<br>Average Sleep Quality: ${sleep.avgSleepQuality}`
-}
-
+  lastSleep.innerText = `Last Night: ${sleep.latest.hoursSlept}`;
+  weeklySleep.innerText = `Weekly Avg: ${sleep.calculateAvg(sleep.latest.date, "hoursSlept")}`;
+  avgSleep.innerHTML = `Average Hours Slept: ${sleep.avgHoursSlept}<br>Average Sleep Quality: ${sleep.avgSleepQuality}`;
+};
 
 function displayHydrationInfo(id, hydrationRepo) {
   const lastElement = hydrationRepo.hydrationData[hydrationRepo.hydrationData.length-1];
@@ -111,5 +109,5 @@ function displayHydrationInfo(id, hydrationRepo) {
                             ${keys[3]}: ${waterByWeek[keys[3]]} ounces<br>
                             ${keys[2]}: ${waterByWeek[keys[2]]} ounces<br>
                             ${keys[1]}: ${waterByWeek[keys[1]]} ounces<br>
-                            ${keys[0]}: ${waterByWeek[keys[0]]} ounces<br>`
+                            ${keys[0]}: ${waterByWeek[keys[0]]} ounces<br>`;
 };
