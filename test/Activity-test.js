b8670c644224e51dacff22b4b2aafc672cd5996b
Kyle Howard <kylehoward1228@yahoo.com>
2022-05-31 14:43:54 -0600

    Add beforeEach and data for testing

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -1,12 +1,27 @@
 import { expect } from "chai";
 import Activity from "../src/Activity";
 import UserRepository from "../src/UserRepository";
-import activityData from "../src/data/activityData";
-import userData from "../src/data/users"
 
 describe("Activity", () => {
-  let activity = new Activity(activityData);
-  let userRepo = new UserRepository(userData)
+  let activityData;
+  let activity;
+  let userRepo;
+  let userData;
+
+  beforeEach(() => {
+     activityData = [
+      {"userID":1,"date":"2019/06/15","numSteps":3577,"minutesActive":140,"flightsOfStairs":16},
+      {"userID":1,"date":"2019/06/16","numSteps":6637,"minutesActive":175,"flightsOfStairs":36},
+      {"userID":1,"date":"2019/06/17","numSteps":14329,"minutesActive":168,"flightsOfStairs":18},
+      {"userID":1,"date":"2019/06/18","numSteps":4419,"minutesActive":165,"flightsOfStairs":33},
+      {"userID":1,"date":"2019/06/19","numSteps":8429,"minutesActive":275,"flightsOfStairs":2},
+      {"userID":1,"date":"2019/06/20","numSteps":14478,"minutesActive":140,"flightsOfStairs":12},
+      {"userID":1,"date":"2019/06/21","numSteps":6760,"minutesActive":135,"flightsOfStairs":6},
+      {"userID":1,"date":"2019/06/22","numSteps":10289,"minutesActive":119,"flightsOfStairs":6},
+    ]
+    activity = new Activity(activityData);
+    userRepo = new UserRepository(userData)
+  });
 
   it('should be a instance of Activity', () => {
     expect(activity).to.be.an.instanceOf(Activity);
